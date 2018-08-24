1d7e3e111f2669c7389e48f0f6b31c1d812ae8c9
LuigiB <luigib@MacBook-Pro-di-Luigi.local>
2018-08-24 15:31:27 +0200

    improved feign with ribbon clinet

--- sdooauth2-poc/src/main/java/it/luigibifulco/oauth2/poc/server/controller/proxy/CounterServiceProxy.java
+++ sdooauth2-poc/src/main/java/it/luigibifulco/oauth2/poc/server/controller/proxy/CounterServiceProxy.java
@@ -1,9 +1,12 @@
 package it.luigibifulco.oauth2.poc.server.controller.proxy;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "counter-service", path = "/counter-service", url = "localhost:9090")
+@FeignClient(name = "counter-service", path = "/counter-service")
+//url = "localhost:9090")
+@RibbonClient(name = "counter-service")
 public interface CounterServiceProxy {
 
 	@PostMapping("/newRequest")
